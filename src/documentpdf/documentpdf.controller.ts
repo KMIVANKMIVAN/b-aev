553a8bcce0c31a1e22690bf6ccb3015636e35289
KMIVANKMIVAN <kmivankmivan@gmail.com>
2023-11-14 18:20:01 -0400

    commit-14-nov-2023

--- src/documentpdf/documentpdf.controller.ts
+++ src/documentpdf/documentpdf.controller.ts
@@ -16,7 +16,20 @@ import { AuthGuard } from 'src/auth/auth.guard';
 @Controller('documentpdf')
 export class DocumentpdfController {
   constructor(private readonly documentpdfService: DocumentpdfService) {}
+
   @UseGuards(AuthGuard)
+  @Post('upload/:textToReplace')
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFile(
+    @UploadedFile() file: Express.Multer.File,
+    @Param('textToReplace') textToReplace: string,
+    @Res() res: Response, // Agregar el objeto de respuesta como parámetro
+  ) {
+    return this.documentpdfService.guardarPdf(file, textToReplace, res);
+  }
+  //await this.documentpdfService.guardarPdf(file, textToReplace, res);
+
+  /* @UseGuards(AuthGuard)
   @Post('upload/:textToReplace')
   @UseInterceptors(FileInterceptor('file'))
   async uploadFile(
@@ -24,7 +37,7 @@ export class DocumentpdfController {
     @Param('textToReplace') textToReplace: string,
   ) {
     return this.documentpdfService.guardarPdf(file, textToReplace);
-  }
+  } */
   @UseGuards(AuthGuard)
   @Get('/download/:fileName')
   download(@Param('fileName') fileName: string, @Res() res: Response) {
--- src/documentpdf/documentpdf.service.ts
+++ src/documentpdf/documentpdf.service.ts
@@ -16,7 +16,12 @@ export class DocumentpdfService {
     private connection: Connection,
     private httpService: HttpService,
   ) {}
-  async guardarPdf(file: Express.Multer.File, textToReplace: string) {
+
+  async guardarPdf(
+    file: Express.Multer.File,
+    textToReplace: string,
+    res: Response,
+  ) {
     if (file) {
       if (file.mimetype !== 'application/pdf') {
         throw new BadRequestException('El archivo no es un PDF válido');
@@ -29,11 +34,6 @@ export class DocumentpdfService {
       }
 
       const dateTime = this.obtenerFechaYHoraActual();
-      /* const uniqueName = await this.concatenarNombreConFechaHora(
-        dateTime,
-        textToReplace,
-      );
-      const destinationPath = '/home/van369/Documentos/' + uniqueName; */
       const uniqueName = await this.concatenarNombreConFechaHora(
         dateTime,
         textToReplace,
@@ -43,34 +43,152 @@ export class DocumentpdfService {
       try {
         fs.writeFileSync(destinationPath, file.buffer);
         console.log(`PDF guardado en: ${destinationPath}`);
+
+        // Configurar la respuesta con el archivo PDF recién guardado
+        const fileStream = fs.createReadStream(destinationPath);
+        res.setHeader('Content-Type', 'application/pdf');
+        res.setHeader(
+          'Content-Disposition',
+          `attachment; filename=${uniqueName}`,
+        );
+        fileStream.pipe(res); // Enviar el archivo PDF como respuesta
+
         return { success: true, message: 'PDF guardado exitosamente' };
       } catch (error) {
         console.error('Error al guardar el PDF:', error);
-        return { success: false, message: 'Error al guardar el PDF' };
+        throw new BadRequestException('Error al guardar el PDF');
       }
     } else {
       console.log('No se recibió ningún archivo.');
       return { success: false, message: 'No se recibió ningún archivo' };
     }
   }
+  // async guardarPdf(file: Express.Multer.File, textToReplace: string) {
+  //   if (file) {
+  //     if (file.mimetype !== 'application/pdf') {
+  //       throw new BadRequestException('El archivo no es un PDF válido');
+  //     }
+
+  //     if (file.size > 10 * 1024 * 1024) {
+  //       throw new BadRequestException(
+  //         'El archivo excede el tamaño máximo de 10MB',
+  //       );
+  //     }
+
+  //     const dateTime = this.obtenerFechaYHoraActual();
+  //     /* const uniqueName = await this.concatenarNombreConFechaHora(
+  //       dateTime,
+  //       textToReplace,
+  //     );
+  //     const destinationPath = '/home/van369/Documentos/' + uniqueName; */
+  //     const uniqueName = await this.concatenarNombreConFechaHora(
+  //       dateTime,
+  //       textToReplace,
+  //     );
+  //     const destinationPath = `/home/van369/Documentos/${uniqueName}`;
+
+  //     try {
+  //       fs.writeFileSync(destinationPath, file.buffer);
+  //       console.log(`PDF guardado en: ${destinationPath}`);
+  //       return { success: true, message: 'PDF guardado exitosamente' };
+  //     } catch (error) {
+  //       console.error('Error al guardar el PDF:', error);
+  //       return { success: false, message: 'Error al guardar el PDF' };
+  //     }
+  //   } else {
+  //     console.log('No se recibió ningún archivo.');
+  //     return { success: false, message: 'No se recibió ningún archivo' };
+  //   }
+  // }
 
   async downloadFile(fileName: string, res: Response): Promise<void> {
-    // Intenta agregar un sufijo numérico si el archivo ya existe
-    let suffix = 0;
-    let filePath = `/home/van369/Documentos/${fileName}`;
-    const destinationPath = `/home/van369/Documentos/descargador/${fileName}`;
+    const filesDirectory = '/home/van369/Documentos/';
+    const destinationDirectory = '/home/van369/Documentos/descargador/';
 
-    while (!(await this.fileExists(filePath))) {
-      if (suffix === 0) {
-        console.error(`File not found: ${filePath}`);
-        throw new BadRequestException('File not found');
+    try {
+      const filesInDirectory = fs.readdirSync(filesDirectory);
+
+      const matchingFiles = filesInDirectory.filter((file) =>
+        file.includes(fileName),
+      );
+
+      if (matchingFiles.length === 0) {
+        throw new BadRequestException('No se encontró el archivo');
       }
 
-      suffix++;
-      filePath = `/home/van369/Documentos/${this.addSuffixToFileName(
-        fileName,
-        suffix,
-      )}`;
+      let fileToDownload = matchingFiles[0]; // Tomar el primer archivo coincidente
+
+      const filePath = `${filesDirectory}${fileToDownload}`;
+      let destinationPath = `${destinationDirectory}${fileToDownload}`;
+
+      if (await this.fileExists(destinationPath)) {
+        // Lógica para agregar sufijo si el archivo ya existe
+        let suffix = 1;
+        let newFileName = this.addSuffixToFileName(fileToDownload, suffix);
+        let newDestinationPath = `${destinationDirectory}${newFileName}`;
+
+        while (await this.fileExists(newDestinationPath)) {
+          suffix++;
+          newFileName = this.addSuffixToFileName(fileToDownload, suffix);
+          newDestinationPath = `${destinationDirectory}${newFileName}`;
+        }
+
+        fileToDownload = newFileName;
+        destinationPath = newDestinationPath;
+      }
+
+      // Configurar el tipo y la disposición del contenido directamente en la respuesta
+      res.setHeader('Content-Type', 'application/pdf');
+      res.setHeader(
+        'Content-Disposition',
+        `attachment; filename=${fileToDownload}`,
+      );
+
+      // Crear un stream de lectura del archivo y enviarlo como respuesta
+      const fileStream = fs.createReadStream(filePath);
+
+      // Pipe también a la respuesta para enviar al cliente
+      const writeStream = fs.createWriteStream(destinationPath);
+      fileStream.pipe(writeStream);
+
+      fileStream.on('end', () => {
+        res.end();
+      });
+
+      fileStream.on('error', (error) => {
+        console.error('Error during file download:', error);
+        throw new BadRequestException('Error during file download');
+      });
+    } catch (error) {
+      console.error('Error during file download:', error);
+      throw new BadRequestException('Error during file download');
+    }
+  }
+
+  private addSuffixToFileName(fileName: string, suffix: number): string {
+    const fileParts = fileName.split('.');
+    const fileBaseName = fileParts.slice(0, -1).join('.');
+    const fileExtension = fileParts[fileParts.length - 1];
+    return `${fileBaseName}(${suffix})${fileExtension}`;
+  }
+
+  /* async downloadFile(fileName: string, res: Response): Promise<void> {
+    const filePath = `/home/van369/Documentos/${fileName}`;
+    let destinationPath = `/home/van369/Documentos/descargador/${fileName}`;
+
+    if (await this.fileExists(destinationPath)) {
+      let suffix = 1;
+      let newFileName = this.addSuffixToFileName(fileName, suffix);
+      let newDestinationPath = `/home/van369/Documentos/descargador/${newFileName}`;
+
+      while (await this.fileExists(newDestinationPath)) {
+        suffix++;
+        newFileName = this.addSuffixToFileName(fileName, suffix);
+        newDestinationPath = `/home/van369/Documentos/descargador/${newFileName}`;
+      }
+
+      fileName = newFileName;
+      destinationPath = newDestinationPath;
     }
 
     try {
@@ -81,17 +199,23 @@ export class DocumentpdfService {
       // Crear un stream de lectura del archivo y enviarlo como respuesta
       const fileStream = fs.createReadStream(filePath);
 
-      // Cambiar la ruta de destino al escribir el archivo
+      // Pipe también a la respuesta para enviar al cliente
       const writeStream = fs.createWriteStream(destinationPath);
       fileStream.pipe(writeStream);
 
-      // Pipe también a la respuesta para enviar al cliente
-      fileStream.pipe(res);
+      fileStream.on('end', () => {
+        res.end();
+      });
+
+      fileStream.on('error', (error) => {
+        console.error('Error during file download:', error);
+        throw new BadRequestException('Error during file download');
+      });
     } catch (error) {
       console.error('Error during file download:', error);
       throw new BadRequestException('Error during file download');
     }
-  }
+  } */
 
   private fileExists(filePath: string): Promise<boolean> {
     return new Promise((resolve) => {
@@ -99,13 +223,6 @@ export class DocumentpdfService {
         resolve(!err);
       });
     });
-  }
-
-  private addSuffixToFileName(fileName: string, suffix: number): string {
-    const fileParts = fileName.split('.');
-    const fileBaseName = fileParts[0];
-    const fileExtension = fileParts[1];
-    return `${fileBaseName}(${suffix}).${fileExtension}`;
   }
 
   /* async guardarPdf(file: Express.Multer.File) {
